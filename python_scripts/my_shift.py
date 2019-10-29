8ba2f75715a3ba38d038e8785b2de12570df7f4a
StePhan McKillen <stephan.myle@gmail.com>
2019-10-29 22:28:23 +1300

    Update my_shift.py

--- python_scripts/my_shift.py
+++ python_scripts/my_shift.py
@@ -1,8 +1,5 @@
 
-
 today = datetime.datetime.now().date()
-
-#read the Data in
 
 shiftpatten = data.get('shiftpatten')
 name = data.get('name')
@@ -10,27 +7,25 @@ dateStr = data.get('firstdayshift')
 patten = data.get('patten')
 
 # Sort out the Date
+
 dateSplit = dateStr.split("/")
 dateDay = int(dateSplit[0])
 dateMonth = int(dateSplit[1])
 dateYear =  int(dateSplit[2])
 FirstDayShift = datetime.date(dateYear,dateMonth,dateDay)
 
-# Work out Days diff
-DaysDiff = (today - FirstDayShift).days
+DaysDiff = ((today - FirstDayShift).days) + 1
 
-#Split the Shift pattens
 PattenShift = shiftpatten.split("x")
 
-#by adding PattenShift[0] + PattenShift[1] we know how long a shift patten is
 WeekPatten = int(PattenShift[0]) + int(PattenShift[1])
 
 shiftpatten = PattenShift[0] + "x" + PattenShift[1]+ "x" + PattenShift[2]
 
-
 sensorName = "sensor.{}_{}".format(name.replace(" " , "_"),shiftpatten)
 
-# know all we have to do is modulo  which will give us a number between 0 and weekPatten
+# now all we have to do is modulo which will give us a number between 0 and weekPatten
+
 FirstDayShift_Mod = (DaysDiff % WeekPatten)
 
 # work out the logic for on/off
@@ -44,27 +39,35 @@ ThisShift = ""
 if FirstDayShift_Mod >= int(PattenShift[0]):
   areweonoff = 'Off'
   ThisShift = "OFF"
-  
+
 elif FirstDayShift_Mod < int(PattenShift[0]):
   areweonoff = 'on'
   ThisShift = patten[FirstDayShift_Mod-1]
 
 # well as we know what day patten we are in
 # we can work out our next start date.
-nextStartdays = (WeekPatten - FirstDayShift_Mod)
+nextStartdays = (WeekPatten - FirstDayShift_Mod) 
 
-nextstartDate = today + datetime.timedelta(nextStartdays)
+# to mines over this logic if its first day
+# do we show todays date or next shift start
+
+nextstartDate = today + datetime.timedelta(nextStartdays + 1 )
 
 # hard work done put what we know into the sensor
 # attributes So we can use them somewhere else in home assistant
 
+# presence
+
 hass.states.set(sensorName , areweonoff ,
   {
     "icon" : "mdi:calendar-star" ,
-    "friendly_name" : "{}'s {}".format(name, shiftpatten) ,
-    "dayspatten" : "{}".format(FirstDayShift_Mod) ,
-    "thisshift": "{}".format(ThisShift.replace("D","Day").replace("N","Night")),
-    "nextstartdate" : "{}/{}/{}".format(nextstartDate.day,nextstartDate.month,nextstartDate.year)
+    "friendly_name" : "{}'s {}".format(name, "Roster") ,
+    "shift patten" :"{}".format(shiftpatten) ,
+    "patten" : "{}".format(patten.rstrip()) ,
+    "day_of_this_shift_patten" : "{}".format(FirstDayShift_Mod) ,
+    "this_day": "{}".format(ThisShift.replace("D","Day").replace("N","Night")),
+    "next_start_date" : "{}/{}/{}".format(nextstartDate.day,nextstartDate.month,nextstartDate.year) , 
+    "First_Day_Shift_Was" : "{}/{}/{}".format(FirstDayShift.day,FirstDayShift.month,FirstDayShift.year)
   }
 )
 
